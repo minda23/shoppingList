dd0982bdc1302f82c9c9c19178982edd088337c3
lukas <luki.minda@gmail.com>
2025-03-10 19:20:43 +0100

    on icrement show basket

--- src/app/Components/OrderSummary.js
+++ src/app/Components/OrderSummary.js
@@ -11,13 +11,9 @@ const OrderSummary = () => {
     return (
         <div className='your_cart'>
             <p className='cart_title'>Your Cart(0)</p>
-
-
-
-            <img className='empty_cart_image' src="/images/illustration-empty-cart.svg" alt="Empty Cart"></img>
+            <img style={{ width: "66%" }} className='empty_cart_image' src="/images/illustration-empty-cart.svg" alt="Empty Cart"></img>
             <p className='empty_cart_message'>Your added items appear here</p>
-
-        </div>
+        </div >
     );
 };
 
