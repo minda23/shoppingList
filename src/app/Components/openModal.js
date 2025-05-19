0a3bc6dc5d3bb612acce3be1d28f7bf3c2863c3b
lukas <luki.minda@gmail.com>
2025-05-19 19:41:34 +0200

    change of name OrderSummary

--- src/app/Components/openModal.js
+++ src/app/Components/openModal.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useContext } from "react";
 import DataContext from "./dataContext";
-import "./openModal2.css";
+import "./openModal.css";
 
 const OpenModal = ({ onClose }) => {
     if (typeof onClose !== "function") {
@@ -25,58 +25,60 @@ const OpenModal = ({ onClose }) => {
     };
 
     return (
-        <div className="modal-overlay">
-            <div className="modal-box">
-                <div className="modal_items">
-                    <div className="modal_text">
-                        <span className="checked">&#10003;</span>
-                        <h1>Order Confirmed</h1>
-                        <p>We hope you enjoy your food</p>
+        <div className="container">
+            <div className="modal-overlay">
+                <div className="modal-box">
+                    <div className="modal_items">
+                        <div className="modal_text">
+                            <span className="checked">&#10003;</span>
+                            <h1>Order Confirmed</h1>
+                            <p>We hope you enjoy your food</p>
 
-                        {/* Display cart items with quantity > 0 */}
-                        {itemsWithAtLeastOneQuantity.map((item) => (
-                            <div key={item.id}>
-                                <div className="product_info">
-                                    <p className="product_name">{item.name}</p>
-                                    <img
-                                        onClick={() => deleteItem(item.id)} // Delete item when clicked
-                                        className="image"
-                                        src="./images/icon-remove-item.svg"
-                                        alt="remove item"
-                                    />
+                            {/* Display cart items with quantity > 0 */}
+                            {itemsWithAtLeastOneQuantity.map((item) => (
+                                <div key={item.id}>
+                                    <div className="product_info">
+                                        <p className="product_name">{item.name}</p>
+                                        <img
+                                            onClick={() => deleteItem(item.id)} // Delete item when clicked
+                                            className="image"
+                                            src="./images/icon-remove-item.svg"
+                                            alt="remove item"
+                                        />
+                                    </div>
+                                    <div className="total-prize">
+                                        <p className="cart_count">{item.product_qua}x</p>
+                                        <p className="item-prize">@{item.price}</p>
+                                        <p className="item-dollar-prize">${item.price * item.product_qua}</p>
+                                    </div>
                                 </div>
-                                <div className="total-prize">
-                                    <p className="cart_count">{item.product_qua}x</p>
-                                    <p className="item-prize">@{item.price}</p>
-                                    <p className="item-dollar-prize">${item.price * item.product_qua}</p>
-                                </div>
+                            ))}
+
+                            <div className="order">
+                                <p className="order_total_label">Order Total</p>
+                                <p className="order_total_value">
+                                    $
+                                    {itemsWithAtLeastOneQuantity.reduce(
+                                        (total, item) => total + item.price * item.product_qua,
+                                        0
+                                    )}
+                                </p>
                             </div>
-                        ))}
-
-                        <div className="order">
-                            <p className="order_total_label">Order Total</p>
-                            <p className="order_total_value">
-                                $
-                                {itemsWithAtLeastOneQuantity.reduce(
-                                    (total, item) => total + item.price * item.product_qua,
-                                    0
-                                )}
-                            </p>
                         </div>
+
+
                     </div>
-
-
+                    <div className="">
+                        <button
+                            variant="contained"
+                            className="close"// Close the modal when clicked
+                        >
+                            Submit Order
+                        </button>
+                    </div>
                 </div>
-                <div className="">
-                    <button
-                        variant="contained"
-                        className="close"// Close the modal when clicked
-                    >
-                        Submit Order
-                    </button>
-                </div>
+
             </div>
-
         </div>
     );
 };
